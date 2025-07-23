c289b257d6537bd0f90771a2e200f939550d02de
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2025-07-23 20:36:31 +0530

    💫DAY 13 ASSIGNMENT💫

---
+++ react-13/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Real from './components/real'
+import FormValidator from './components/formvalidator'
+
+function App() {
+
+  return ( 
+    <FormValidator />
+  )
+}
+
+export default App
