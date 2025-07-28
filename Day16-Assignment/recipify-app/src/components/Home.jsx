281a62993236b12fe26755f3cddcaf0d4031bb03
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2025-07-28 07:07:34 +0530

    💫DAY 16 ASSIGNMENT💫

--- Day16-Assignment/recipify-app/src/components/Home.jsx
+++ Day16-Assignment/recipify-app/src/components/Home.jsx
@@ -3,12 +3,18 @@ import './Home.css'
 import image1 from '../assets/image1.jpg'
 import image2 from '../assets/image2.jpg'
 import image3 from '../assets/image3.jpg'
+import image4 from '../assets/image4.jpg'
+import image5 from '../assets/image5.jpg'
+import image6 from '../assets/image6.jpg'
 
 function Home() {
   const sliderImages = [
     image1,
     image2,
     image3,
+    image4,
+    image5,
+    image6
   ];
 
   const [cardData, setCardData] = React.useState([]);
